6b70c03e97881c2edec257faad084c9442df0a49
DanielNeves <dbenditoneves@gmail.com>
2020-08-22 15:31:39 -0300

    MVP2 semi-pronto, só faltando pré-requisitos

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
   resources :themes
   resources :teachers
   resources :students
+  resources :users
   get '/sign_up', to: 'users#index'
   post '/sign_up', to: 'users#create'
   post '/login', to: 'users#login'
   post '/themes', to: 'users#add_theme' #Rota para add matéria se o usuário for confirmado como secretária ou admin
   get '/themes', to: 'users#index_themes' #Rotas para mostrar todas as matérias que podem-se dar aulas depois de adicionadas
-  resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
