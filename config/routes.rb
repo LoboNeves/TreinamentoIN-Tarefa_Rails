d0d31a82178d80729b3875d5fdb29417f98792f2
DanielNeves <dbenditoneves@gmail.com>
2020-08-21 22:49:43 -0300

    Passado as instruções de register controller e session controller para users controller
    E deixado os controllers register e session sem uso por enquanto, tendo mudado as rotas em routers.rb para as funções do users controller também ao ver no documento que toda a lógica de login deve estar vinculada ao usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  post '/sign_up', to: 'register#sign_up'
-  post '/login', to: 'session#login'
+  post '/sign_up', to: 'users#create'
+  post '/login', to: 'users#login'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
